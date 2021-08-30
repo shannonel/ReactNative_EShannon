bd76c9f9cf15e12fe764399007016bea769fa147
shannonel <85324902+shannonel@users.noreply.github.com>
2021-08-29 23:23:23 -0400

    Week 1 Assignment Task 1

---
+++ components/AboutComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+class About extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    static navigationOptions = {
+        title: 'About Us'
+    }
+
+    render() {
+        return (
+            <ScrollView></ScrollView>
+        )
+    }
+}
+
+export default About;
---
+++ components/ContactComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+class Contact extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+
+    render() {
+        return (
+            <ScrollView></ScrollView>
+        )
+    }
+}
+
+export default Contact;
