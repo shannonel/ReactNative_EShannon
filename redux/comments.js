772118db32743ba53f195e5587699b9ea70f8542
shannonel <85324902+shannonel@users.noreply.github.com>
2021-09-04 13:15:30 -0400

    Week 2 Assignment Task 3 NOT CORRECT

--- redux/comments.js
+++ redux/comments.js
@@ -8,6 +8,12 @@ export const comments = (state = { errMess: null, comments: []}, action) => {
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload};
 
+        case ActionTypes.ADD_COMMENT:
+            let comment = action.payload;
+            comment.id = this.state.comments.length;
+            action.payload.id = this.state.comments.length;
+            return {...state, errMess: null, comments: action.payload};
+
         default:
             return state;
     }
